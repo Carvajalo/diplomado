688dbc4c69bfb80612cbe6865f047d4db4bd9d39
Carvajalo <carvajalo@uninorte.edu.co>
2023-10-25 23:59:46 -0500

    Remove admin role to signup

--- sprint4/src/routes/user.routes.js
+++ sprint4/src/routes/user.routes.js
@@ -1,12 +1,11 @@
 import { Router } from 'express';
 import * as userController from '../controllers/users.controller.js';
-import * as authMiddleware from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
 router.get('/', userController.getAllUsers);
 
-router.post('/signup', authMiddleware.roleValidation, userController.createUser);
+router.post('/signup', userController.createUser);
 
 router.get('/:userId', userController.getUserById);
 
