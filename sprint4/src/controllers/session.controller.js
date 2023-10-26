4c15bfb54789de833dd44dfb384cc5832732e532
Carvajalo <carvajalo@uninorte.edu.co>
2023-10-25 23:58:06 -0500

    validateAdmin role middleware

--- sprint4/src/controllers/session.controller.js
+++ sprint4/src/controllers/session.controller.js
@@ -1,9 +1,12 @@
 import User from '../models/user.js';
 import { generateToken } from '../services/jwt.service.js';
+import { verifyToken } from '../services/jwt.service.js';
+import { newError } from '../utils/error.utils.js';
 
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
+    console.log(req.body)
     if (!email || !password) return res.status(400).json({ message: 'Email and Password are required' });
     const user = (await User.findOne({ email })) || (await User.findOne({ name: email }));
     if (!user) return res.status(404).json({ message: 'Email or Username incorrect' });
@@ -13,6 +16,22 @@ export const login = async (req, res) => {
     const payload = { id: user._id, role, name };
     const token = generateToken({ payload });
     return res.status(200).json({ token, role: user.role });
+  } catch (e) {
+    res.status(400).json({ message: e.message, line: e.stack });
+  }
+}
+
+export const validateAdmin = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) return res.status(400).json({ message: 'Token is required' });
+    const token = authorization.split(' ')[1];
+    const { role } = verifyToken({ token });
+    console.log({role})
+    if (role !== "admin") return res.status(401).json({ message: 'Unauthorized' });
+
+    req.role = role;
+    next();
   } catch (e) {
     res.status(400).json({ message: e.message, line: e.stack });
   }
